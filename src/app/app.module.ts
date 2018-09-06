d08cdfbfb8acb81644572d7fb8072c0f24ba5d07
Pawel <paw.mastalerz@gmail.com>
2018-09-06 09:24:38 +0200

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderContactComponent } from './header/header-contact/header-contact.component';
 import { HeaderLogoComponent } from './header/header-logo/header-logo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeaderContactComponent,
-    HeaderLogoComponent
+    HeaderLogoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
