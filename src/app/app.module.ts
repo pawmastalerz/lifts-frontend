9c7a9aebd20f9e58e6b936c9679f16b06f212c76
Pawel <paw.mastalerz@gmail.com>
2018-09-06 15:22:22 +0200

    Carousel beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { HeaderContactComponent } from './header/header-contact/header-contact.component';
 import { HeaderLogoComponent } from './header/header-logo/header-logo.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     HeaderComponent,
     HeaderContactComponent,
     HeaderLogoComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
