67a03959752885c81678dfab0972be7fde70f5c3
Pawel <paw.mastalerz@gmail.com>
2018-09-07 12:36:48 +0200

    Header and navbar

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,6 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./src'));
 });
 
-// gulp.task('combine-css', function() {
-//   return gulp.src([
-//       './node_modules/owl.carousel/dist/assets/owl.carousel.min.css',
-//       './node_modules/owl.carousel/dist/assets/owl.theme.default.min.css',
-//       './src/styles.css',
-//       ])
-//     .pipe(concat('style.css'))
-//     .pipe(gulp.dest('./src'));
-// });
-// gulp.task('combine-js', function() {
-//   return gulp.src([
-//       './node_modules/owl.carousel/dist//owl.carousel.min.css',
-//       ])
-//     .pipe(concat('style.css'))
-//     .pipe(gulp.dest('./src'));
-// });
-
 gulp.task('sass:watch', function () {
   gulp.watch('./src/**/*.scss', ['sass']);
-  gulp.watch('./src/styles.css', ['combine-css']);
 });
-
-
-// gulp.task('default', ['combine-css', 'sass:watch','sass'], function() {});
